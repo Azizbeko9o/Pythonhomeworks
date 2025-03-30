569d3fa1c21506cd0f5d2a62dacac7db2dab6d70
Azizbeko9o <umidovicho9o@gmail.com>
2025-03-30 23:49:01 +0500

    Add files via upload

---
+++ lesson-1/lesson-1.py
@@ -0,0 +1,28 @@
+#a = 'Azizbek'
+#print(len('azizbek'))
+#number
+#print(type(1)) #int - integer - butun son
+#print(type(2)) #int - integer - butun son
+#print(type(1.5)) #float - Float - Haqiqiy son
+#print(type(2 + 3j)) # comples - complex -murakkab son
+#print(type('Hello')) #str - String - So'z
+#print(type('True')) #str - String - So'z
+#print(type(True)) #bool - BooLean 
+#print(type(False)) #bool - BooLean 
+#print("Apple " * 5) # str * int = 
+# a = 5
+# b = 6
+ 
+# print(a + b) 
+# print(a - b) 
+# print(a * b)
+# print(a / b)
+a = int(input("a = "))
+b = int(input("b = "))
+print(type(a))
+
+a = int(a)
+b = int(b)
+print(type(a))
+c = a + b
+print('a + b =', c)
