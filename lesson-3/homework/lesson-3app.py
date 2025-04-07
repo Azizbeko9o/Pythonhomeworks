9c015dae7ef03dd6e75e91586589fe10b3b231f3
Azizbeko9o <umidovicho9o@gmail.com>
2025-04-07 18:01:13 +0500

    Add files via upload

---
+++ lesson-3/homework/lesson-3app.py
@@ -0,0 +1,9 @@
+#word = input("Enter word: ")
+# word = 'apple'
+# word_reverse = word[::-1]
+# print(f'{word_reverse=}')
+# print(word == word_reverse)
+# word = input("Bitta soz kiriting: ")
+# word_reverse = word[::-1]
+# print(f"{word_reverse=}")
+# print(word == word_reverse)
